4747f891993c8728043951f20c1250df414f48ac
Shockch4rge <favouriteteo@gmail.com>
2022-04-16 22:54:27 +0800

    More consistent event names

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { Server } from "socket.io";
 import { PrismaClient } from "@prisma/client";
 
 import { ClientEvents, ServerEvents } from "./typings/events";
+import { createMockData } from "./utils/createMockData";
 
 
 const prisma = new PrismaClient({
@@ -21,30 +22,90 @@ async function main() {
 			// url of client
 			origin: "http://localhost:3000",
 			methods: ["GET", "POST", "PUT"],
+			credentials: true,
 		},
 	});
+
+	// TODO:
+	await createMockData(prisma);
+
+	const allusers = await prisma.user.findMany();
+	const allrooms = await prisma.room.findMany();
+	console.log("users", allusers);
+	console.log("rooms", allrooms);
 
 	io.on("connection", socket => {
 		console.log(`${socket.id} connected`);
 
-		socket.on("messageCreate", async message => {
-			console.log(`[messageCreate] ID: ${socket.id}`);
-			await prisma.message.create({
-				data: message,
-			});
+		socket.on("createMessage", async data => {
+			const message = await prisma.message.create({
+				data: {
+					roomId: data.roomId,
+					authorId: data.authorId,
+					content: data.content,
+				},
 
-			socket.emit("messageReceived", message);
-		});
-
-		socket.on("messageDelete", async message => {
-			console.log(`[messageDelete]: ID: ${socket.id}`);
-			await prisma.message.delete({
-				where: {
-					id: message.id,
+				include: {
+					author: true,
+					room: true,
 				},
 			});
 
-			socket.emit("messageDeleted", message);
+			socket.emit("createdMessage", message);
+		});
+
+		socket.on("requestAllRooms", async () => {
+			const rooms = await prisma.room.findMany({
+				include: {
+					messages: true,
+				},
+			});
+
+			socket.emit("sentAllRooms", rooms);
+		});
+
+		socket.on("requestAllRoomMessages", async roomId => {
+			const messages = await prisma.message.findMany({
+				where: {
+					roomId,
+				},
+				include: {
+					author: true,
+				},
+			});
+
+			socket.emit("sentAllRoomMessages", messages);
+		});
+
+		// all rooms with corresponding messages in them
+		// requestClient
+		socket.on("requestClientData", async clientId => {
+			const user = await prisma.user.findUnique({
+				where: {
+					id: clientId,
+				},
+				include: {
+					rooms: {
+						where: {
+							users: {
+								some: {
+									id: clientId,
+								},
+							},
+						},
+
+						include: {
+							messages: true,
+						},
+					},
+				},
+				// false for now
+				rejectOnNotFound: false,
+			});
+
+			if (user !== null) {
+				socket.emit("sentClientData", user);
+			}
 		});
 
 		socket.on("disconnect", () => {
